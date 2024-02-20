cfc6a88a87314abe33f8821806cb0cd95b4e50f7
Abhilash Singh <abhilash4597@gmail.com>
2024-02-20 22:10:27 +0530

    update

--- src/Components/Accordian/index.jsx
+++ src/Components/Accordian/index.jsx
@@ -3,14 +3,34 @@ import data from "./data";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiSelection, setMultiSelection] = useState([]);
 
   function handleSingleSelection(currentId) {
     setSelected(currentId === selected ? null : currentId);
   }
 
+  function handleMultiSelection(currentId) {
+    let copyMultiple = [...multiSelection];
+    let findIndexOfCurrentId = copyMultiple.indexOf(currentId);
+
+    if (findIndexOfCurrentId === -1) {
+      copyMultiple.push(currentId);
+    } else {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+
+    setMultiSelection(copyMultiple);
+  }
+
   return (
     <div className="wrapper">
-    <button>Enable Multi Selection</button>
+      <button
+        onClick={() => {
+          setEnableMultiSelection(!enableMultiSelection);
+        }}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => {
@@ -18,11 +38,16 @@ export default function Accordian() {
               <div className="item" key={dataItem.id}>
                 <div
                   className="title"
-                  onClick={() => handleSingleSelection(dataItem.id)}>
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }>
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
-                {selected === dataItem.id ? (
+                {selected === dataItem.id ||
+                multiSelection.indexOf(dataItem.id) !== -1 ? (
                   <p className="content">{dataItem.answer}</p>
                 ) : null}
               </div>

dc0c780d407fed40fb6d617dbae8c9ee1e7d7d06
Abhilash Singh <abhilash4597@gmail.com>
2024-02-21 23:54:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Accordian from "./Components/Accordian";
+import RandomColor from "./Components/Random-color";
 
 function App() {
   return (
     <div>
       {/* Accordian Component */}
       <Accordian />
+      {/* Random_color Component */}
+      <RandomColor />
     </div>
   );
 }
---
+++ src/Components/Random-color/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function RandomColor() {
+  return <div className="container">Generate Random Color</div>;
+}

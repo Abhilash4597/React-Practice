c2d45b5f8c249c39afec8b50dc162e83e208b738
Abhilash Singh <abhilash4597@gmail.com>
2024-03-06 16:37:56 +0530

    update

--- src/Components/Image-Slider/index.jsx
+++ src/Components/Image-Slider/index.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Image-Slider/styles.css";
 
 export default function ImageSlider({ url, limit }) {
-  const [item, setItem] = useState("");
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (url !== "") {
+      fetchImages(url);
+    }
+  }, [url]);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return <div className="loading">Loading Data !! Please Wait.</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div className="error">Error Occured !! {errorMsg}</div>;
+  }
 
   return <div className="sliderComponent">ImageSlider</div>;
 }

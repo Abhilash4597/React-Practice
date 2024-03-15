152571c202872d296865032f466fbaf14ae09eaf
Abhilash Singh <abhilash4597@gmail.com>
2024-03-15 22:58:27 +0530

    update

--- src/Components/Load-More/index.jsx
+++ src/Components/Load-More/index.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "../Load-More/styles.css";
 
 export default function LoadMore() {
+  const [load, setLoad] = useState(0);
+
+  function handleClick() {
+    setLoad(load + 1);
+  }
   return (
     <div className="loadMore_Container">
-      <div className="loadMore">LoadMore</div>
+      <div className="loadMore" onClick={handleClick}>
+        {load}
+      </div>
     </div>
   );
 }

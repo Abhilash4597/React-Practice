253121fc47ca16b32c60b82e2f9a5d89cc3c930d
Abhilash Singh <abhilash4597@gmail.com>
2024-03-29 22:55:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import Accordian from "./Components/Accordian";
-import RandomColor from "./Components/Random-color";
-import StarRating from "./Components/Star-Rating";
-import ImageSlider from "./Components/Image-Slider";
+// import Accordian from "./Components/Accordian";
+// import RandomColor from "./Components/Random-color";
+// import StarRating from "./Components/Star-Rating";
+// import ImageSlider from "./Components/Image-Slider";
 import LoadMore from "./Components/Load-More";
 
 function App() {
   return (
     <div>
       {/* Accordian Component */}
-      <Accordian />
+      {/* <Accordian /> */}
 
       {/* Random-color Component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
 
       {/* Star-Rating Component */}
-      <StarRating noOfStar={5} />
+      {/* <StarRating noOfStar={5} /> */}
 
       {/* Image-Slider Component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={2} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={2} /> */}
 
       {/* Load-More Component */}
       <LoadMore />
